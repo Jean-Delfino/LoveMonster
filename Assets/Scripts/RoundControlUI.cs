7060a98db3927e85ee4347e83975671ac5c0ee49
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-17 17:30:30 -0300

    UI for Round Control.

---
+++ Assets/Scripts/RoundControlUI.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RoundControlUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI roundCounter;
+    [SerializeField] private TextMeshProUGUI monsterCounter;
+    [SerializeField] private TextMeshProUGUI remainingMonsterCounter;
+    [SerializeField] private TextMeshProUGUI roundTimeCounts;
+    
+    public void Setup(int rounds, int monsterAmount)
+    {
+        monsterCounter.text = $"{monsterAmount}";
+        roundCounter.text = $"{rounds}";
+    }
+
+    public void SetRemainingMonster(int remainingMonsters)
+    {
+        remainingMonsterCounter.text = $"{remainingMonsters}";
+    }
+
+    public void UpdateTimer(float totalTime)
+    {
+        roundTimeCounts.text = $"{totalTime}";
+    }
+}
