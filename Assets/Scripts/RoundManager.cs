3e2b79fe115a6aee45ef3c7a6da5cb487a0b4307
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-17 15:15:35 -0300

    First idea of the RoundManager.

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    private int _currentRoundCountdown = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    private IEnumerator ProcessRounds()
+    {
+        while (enabled)
+        {
+            yield return null;
+            _currentRoundCountdown++;
+            
+            var monsterAmount = UtilMath.GetFibonacciRecursively(_currentRoundCountdown);
+            var monsterRenderers = new Renderer[monsterAmount];
+            
+            RecursivelySpawnAllCreatures(monsterRenderers, monsterAmount);
+            yield return new WaitUntil(AllCreaturesAreNotVisible);
+        }
+    }
+
+    private void RecursivelySpawnAllCreatures(Renderer[] monster, int n)
+    {
+        if(n < 0) return;
+        
+        //Instantiate with pooling
+        //Add to monster array
+        RecursivelySpawnAllCreatures(monster, n - 1);
+    }
+
+    private bool AllCreaturesAreNotVisible()
+    {
+        return true;
+    }
+}
